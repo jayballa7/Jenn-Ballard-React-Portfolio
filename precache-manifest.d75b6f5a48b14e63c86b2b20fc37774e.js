30eff1d90b74914512d93d32198e1e1b963c848b
Jennifer Ballard <jenn_ballard7@hotmail.com>
2020-06-03 18:44:10 -0700

    Updates

---
+++ precache-manifest.d75b6f5a48b14e63c86b2b20fc37774e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb7dfe41541691ce6aa4c99afe2303a",
+    "url": "/Jenn-Ballard-React-Portfolio/index.html"
+  },
+  {
+    "revision": "89e5c7e7d14a11c72c60",
+    "url": "/Jenn-Ballard-React-Portfolio/static/css/main.ff3a8082.chunk.css"
+  },
+  {
+    "revision": "b39d9261da1f778a1b4c",
+    "url": "/Jenn-Ballard-React-Portfolio/static/js/2.0d688b6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Jenn-Ballard-React-Portfolio/static/js/2.0d688b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e5c7e7d14a11c72c60",
+    "url": "/Jenn-Ballard-React-Portfolio/static/js/main.1c11213b.chunk.js"
+  },
+  {
+    "revision": "fad17c8e0abed453bab5",
+    "url": "/Jenn-Ballard-React-Portfolio/static/js/runtime-main.27f59af7.js"
+  },
+  {
+    "revision": "c8e0dbe55f1f90026171727c5e756954",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/astronaut.c8e0dbe5.jpg_fit=scale"
+  },
+  {
+    "revision": "9f3c5b80a2253bcf6393143f83df01a5",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/crimeradar.9f3c5b80.PNG"
+  },
+  {
+    "revision": "45a95aa4ffa2280dce6fc3186a91cb34",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/eatdaburger.45a95aa4.png"
+  },
+  {
+    "revision": "dea38a93417f6c86ef04c10275108862",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/employeetracker.dea38a93.png"
+  },
+  {
+    "revision": "8daad60833801640685e57e05d4e3fc9",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/newsflash.8daad608.PNG"
+  },
+  {
+    "revision": "5b425f56d74b1d9e7fa5a2ff6a54aed3",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/profile-photo.5b425f56.jpg"
+  },
+  {
+    "revision": "1c374c4d0114b5bc33716f03a368fcc9",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/rendezvous.1c374c4d.PNG"
+  },
+  {
+    "revision": "de2225ff974479a084d64d905dc1172a",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/star-background.de2225ff.jpg"
+  },
+  {
+    "revision": "fb71a5cd1bc74abf1b4ae2c46b3069cb",
+    "url": "/Jenn-Ballard-React-Portfolio/static/media/weatherdashboard.fb71a5cd.PNG"
+  }
+]);
