f88d04434a60e6e10fc39601057695e35b7e4ae6
Jennifer Ballard <jenn_ballard7@hotmail.com>
2020-06-03 17:27:38 -0700

    contact page complete

--- jenn-ballard-react-portfolio/src/components/About.js
+++ jenn-ballard-react-portfolio/src/components/About.js
@@ -15,7 +15,7 @@ function About() {
                 </div>
                 <div className = "about-text">
                     <p>I am a Full Stack Web Developer currently settled in the greater Seattle area. In May of 2020, I received a Certificate from the University of Washington Coding Bootcamp. I specialize in Virtual Studio Code, Git Bash, GitHub, HTML, CSS, CSS Grid, Sass, Bootstrap, JavaScript, jQuery, Node.js, Express, Mysql, MongoDB, and React. My favorite part about web development is designing the user interface. </p>
-                    <p>In my free time I enjoy going to concerts, playing soccer, watching sports (Go Hawks!), and spending time with my two new kittens, Taco & Tequila.</p>
+                    <p>In my free time I enjoy going to concerts, watching movies, playing soccer, watching sports (Go Hawks!), and spending time with my two new kittens, Taco & Tequila. A dream of mine is to become an astronaut.</p>
                 </div>
             </div>
             <div className = "footer">
--- jenn-ballard-react-portfolio/src/components/Contact.js
+++ jenn-ballard-react-portfolio/src/components/Contact.js
@@ -1,3 +1,30 @@
+// import React from 'react';
+// import '../styles/Contact.scss';
+// import Header from "../components/Header.js";
+// import Footer from "../components/Footer.js";
+
+
+// function Contact() {
+//     return(
+//         <div className="contact-wrapper">
+//             <Header />
+//             <div className = "moonbox">
+//                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" className = "moon" alt=""></img>
+//             </div>
+//             <div className = "contact-card">
+//                 <h1>Contact Information</h1>
+//                 <p>Phone Number: 425-443-2792</p>
+//                 <p>Email: jenn_ballard7@hotmail.com</p>
+//                 <p>Located in Renton, Washington</p>
+//             </div>
+//             <div className = "contact-footer">
+//                 <Footer />
+//             </div>
+//         </div>
+//     )
+// }
+// export default Contact;
+
 import React from 'react';
 import '../styles/Contact.scss';
 import Header from "../components/Header.js";
@@ -8,9 +35,22 @@ function Contact() {
     return(
         <div className="contact-wrapper">
             <Header />
-            <div className = "contact-card">
+            <div className="contact-content">
+            <div className = "moonbox">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" className = "moon" alt=""></img>
             </div>
-            <Footer />
+            <div className = "contact-card">
+                <h2>Contact Information</h2>
+                <div className="separator"></div> 
+                <h3>Jennifer Ballard</h3>
+                <p>Phone Number: 425-443-2792</p>
+                <p>Email: jenn_ballard7@hotmail.com</p>
+                <p>Located in Renton, Washington</p>
+            </div>
+            <div className = "contact-footer">
+                <Footer />
+            </div>
+            </div>
         </div>
     )
 }
